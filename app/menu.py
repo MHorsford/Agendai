b3b78c5829b59ab8e3eb9eb2b2e40b673955ad92
Marcos Horsford <horsforddeveloper@gmail.com>
2023-07-31 01:31:38 -0300

    Primeira versão do AgendAí

---
+++ app/menu.py
@@ -0,0 +1,37 @@
+from app.manager import Manager
+import os
+
+
+def select_option():
+    choise = str(input('1 - Adicionar uma tarefa\n'
+                       '2 - Verificar tarefas\n'
+                       '3 - Modificar uma tarefa\n'
+                       '4 - Excluir uma tarefa\n'
+                       '5 - Inicie\n'
+                       '0 - Sair/Salvar\n'
+                       'Selecione a opção: '))
+    return choise
+
+
+taskManage = Manager()
+file = os.path.isfile('savetask.csv')
+if not file:
+    taskManage.save_task('savetask.csv')
+
+while True:
+
+    choise = select_option()
+
+    if choise == '1':
+        taskManage.add_task()
+        taskManage.save_task('savetask.csv')
+    elif choise == '2':
+        taskManage.verify()
+    elif choise == '3':
+        taskManage.modify_task()
+    elif choise == '4':
+        taskManage.delete_task()
+    elif choise == '5':
+        taskManage.alarm_shot()
+    elif choise == '0':
+        break
